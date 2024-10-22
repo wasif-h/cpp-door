508577b7c203d0bd1e631716fd36586b8f1d8fa7
Wasif Hossain <wasif.hx@gmail.com>
2024-10-22 22:03:14 +0600

    update

--- Basic/pointer.cpp
+++ Basic/pointer.cpp
@@ -3,6 +3,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Pass by Value
+void change_a_func1(int a)
+{
+
+    a = 10;
+}
+
+// Pass by reference using alias
+void change_a_func2(int &a)
+{
+    a = 20;
+}
+
+// Pass by reference using pointer
+
+void change_a_func3(int *a)
+{
+    *a = 30;
+}
+
 // <<<---------  main function   --------->>>
 int main()
 {
@@ -16,13 +36,13 @@ int main()
 
     // pointer variable - to store the mem address of a varibale we use a  special variable called pointer variable
 
-    int* ptra = &a;
+    int *ptra = &a;
     cout << "3. value of ptra = " << ptra << endl;
 
     // pointer to pointer - to store the address of pointer variable into another pointer variable
     // int* is the data type of ptra and int** indicate this as a pointer variable of ptra which is partent pointer variable
 
-    int** parptra = &ptra;
+    int **parptra = &ptra;
 
     cout << "4. address of ptra = " << &ptra << endl;
     cout << "5. Value of parptra = " << parptra << endl;
@@ -33,7 +53,21 @@ int main()
     cout << "6. Value of ptra = " << val_of_ptra << endl;
     int val_of_parptra = **(parptra);
     cout << "7. Value of parptra = " << val_of_parptra << endl;
-    
+
+    // Here is function part ---------------
+    cout << "------------ Function Part --------------" << endl;
+
+    a = 7;
+    cout << a << endl;
+
+    change_a_func1(a);
+    cout << a << endl;
+
+    change_a_func2(a);
+    cout << a << endl;
+
+    change_a_func3(&a);
+    cout << a << endl;
 
     return 0;
 }
